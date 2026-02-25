c61a6971c4ac660332af088f32bc7a9b9797faba
dlk3 <dave@daveking.com>
2026-02-25 17:01:41 -0500

    Minor documentation tweaks

--- lib/autokey/scripting/__init__.py
+++ lib/autokey/scripting/__init__.py
@@ -13,6 +13,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+"""
+This package contains the scripting API.
+
+This file centralises the public API classes for easier importing.
+"""
+
 import autokey.common
 
 from . import highlevel
