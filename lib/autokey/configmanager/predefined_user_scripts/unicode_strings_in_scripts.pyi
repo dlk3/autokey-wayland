8d1466b3a18baf4573e1365c88df11931d4aa9e0
dlk3 <dave@daveking.com>
2026-02-27 09:08:39 -0500

    Add back in the sample scripts that depend on the clipboard API

--- lib/autokey/configmanager/predefined_user_scripts/unicode_strings_in_scripts.pyi
+++ lib/autokey/configmanager/predefined_user_scripts/unicode_strings_in_scripts.pyi
@@ -6,14 +6,21 @@ import os
 #  If the script is running in a Wayland environment ...
 if os.environ['XDG_SESSION_TYPE'] == 'wayland':
 
-    # then it can output a string containing a Unicode character like this:
-    character = '🥷'
-    escape_code = character.encode('unicode_escape').decode('utf-8').lstrip('\\U0')
-    keyboard.send_keys(f'This is the "ninja" character: <ctrl>+<shift>+u{escape_code} ', delay=50)
-    time.sleep(0.3)   # Typing thread needs time to finish up before we send more
+    # we'll create a function to prevent having to write duplicate
+    # code when we need to handle more than just one character
+    def getEscapeCode(character):
+        escape_code = character.encode('unicode_escape').decode('utf-8').lstrip('\\U0')
+        return f'<ctrl>+<shift>+u{escape_code}+<enter>'
+    
+    # then we can output a string containing a Unicode character like this:
+    keyboard.send_keys(f'This is the "ninja" character: {getEscapeCode('🥷')}')
+    #  the Unicode character needs to be the last character in the string, and
+    #  then we need to take a short break to let the typing thread catch up 
+    #  before sending more text.
+    time.sleep(0.3)
     keyboard.send_keys(' Scary!')
      
 else:
 
-    #  otherwise, this must be an X11 environment and the script can simply do this: 
+    #  otherwise, this must be an X11 environment and the script can simply be this: 
     keyboard.send_keys('This the "ninja" character: 🥷 Scary!')
