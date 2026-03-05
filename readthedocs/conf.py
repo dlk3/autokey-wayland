0fea7be3a98bb55d188ec2764cf76c934a87977d
dlk3 <dave@daveking.com>
2026-03-05 02:04:11 -0500

    Add script to build documentation site locally for testing

--- readthedocs/conf.py
+++ readthedocs/conf.py
@@ -79,12 +79,12 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 html_logo = 'autokey.png'
 html_theme_options = {
     'logo_only': True,
 }
-html_favicon = 'favicon.ico'
+#html_favicon = 'favicon.ico'
 
 #TODO make this point to wherever the source files are going to be hosted
 # this enables the "edit on github" behavior for the top right corners of web pages
